f02e8af4bb79486e2607c26f1065d7ac0ce5bd48
eduardo (lab) <eduardogladzik@gmail.com>
2025-10-21 10:57:33 -0300

    path to update by url request removed

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -6,6 +6,5 @@ from . import views
 urlpatterns = [
     path('habilidades', views.get_abilities),
     path('habilidade:<str:name>', views.get_ability),
-    path('update:<str:name>', views.update_ability),
     path('api/', views.ability_manager),
 ]
