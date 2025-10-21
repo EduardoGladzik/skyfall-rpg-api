bbae7c892e6ce9a07700217a9baedd20e29c3183
eduardo (lab) <eduardogladzik@gmail.com>
2025-10-21 10:56:21 -0300

    bug: variables 'execution_type' and 'duration' are not required to register a new ability and do not show on the register panel.

--- backend/api/models/ability.py
+++ backend/api/models/ability.py
@@ -3,7 +3,7 @@ from django.db import models
 class Ability(models.Model):
 
     source = models.CharField(max_length=50, default='',)
-    execution_type = models.TextChoices('Ação', 'Ação Bonus', 'Reação', 'Ação Livre', 'Passiva', 'Mais que uma Ação')
+    execution_type = models.Choices('Ação', 'Ação Bonus', 'Reação', 'Ação Livre', 'Passiva', 'Mais que uma Ação')
     name = models.CharField(primary_key=True, max_length=50, default='')
     cost = models.IntegerField(default=0)
     descriptors = models.CharField(max_length=50, default='') # Procurar maneiras de criar uma lista de opções onde seja possível adicionar mais de uma opção à variável
@@ -11,7 +11,7 @@ class Ability(models.Model):
 
     range = models.CharField(max_length=50, default='')
     target = models.CharField(max_length=50, default='')
-    duration = models.TextChoices('Instantânea', 'Concentração')
+    duration = models.Choices('Instantânea', 'Concentração')
     attack = models.CharField(max_length=50, default='')
     trigger = models.CharField(max_length=50, default='') #tornar opcional
 
