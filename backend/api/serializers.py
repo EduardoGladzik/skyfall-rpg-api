4c1633694021f9bc104b2f4ffd0042ecc5017eba
Eduardo Gladzik <eduardogladzik@gmail.com>
2026-03-20 15:00:34 -0300

    Serializers modified for better frontend function

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -2,14 +2,32 @@ from rest_framework import serializers
 from .models.models import Ability
 from .models.models import Spell
 from .models.models import Descriptor
-
+    
 class AbilitySerializer(serializers.ModelSerializer):
+    
+    descriptors = serializers.SlugRelatedField(
+        many=True,
+        read_only=True,
+        slug_field='name'
+    )
     class Meta:
         model = Ability
         fields = '__all__'
 
 
 class SpellSerializer(serializers.ModelSerializer):
+    
+    components = serializers.SlugRelatedField(
+        many=True,
+        read_only=True,
+        slug_field='name'
+    )
+    
+    descriptors = serializers.SlugRelatedField(
+        many=True,
+        read_only=True,
+        slug_field='name'
+    )
     class Meta:
         model = Spell
         fields = '__all__'
